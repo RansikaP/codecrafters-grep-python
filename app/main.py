d33256dd0d2bcba3dee0db1971d145d195cb6964
Ransika <57299043+RansikaP@users.noreply.github.com>
2024-09-05 16:28:54 -0400

    13

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ def backreference(input, pattern):
     matches = re.findall(patterns[0][1:len(patterns[0])-1], input)
     print(matches)
     print(int(patterns[2][1:]), len(matches) + 1)
-    if int(patterns[2][1:]) == len(matches) + 1:
+    if int(patterns[2][1:]) + 1 == len(matches):
         return 0
     else: return 1
     
