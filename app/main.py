585ec8b5fa2f0a578ea9093567a4906507342a10
Ransika <57299043+RansikaP@users.noreply.github.com>
2024-09-05 16:01:17 -0400

    7

--- app/main.py
+++ app/main.py
@@ -39,8 +39,8 @@ def main():
 
     # Uncomment this block to pass the first stage
     if pattern.count('\\') > 1:
-        exit(combined_char(input_line, pattern))            
-    elif pattern.startswith('\\') or re.search("^\[.*\]$", pattern):
+        exit(combined_char(input_line, pattern))
+    elif pattern.startswith('\\') or re.search("^\[.*\]$", pattern) or pattern.startswith('^'):
         exit(match_d_an_cg(input_line, pattern))
     elif match_pattern(input_line, pattern):
         exit(0)
